74b0fed14cc8378a33287b09a3b29b2c2eb957d7
Dmitry Spasibenko <dmitry@Dmitrys-MacBook-Pro.local>
2017-11-27 18:43:14 -0800

    bytes_buf

---
+++ bytes_buf.go
@@ -0,0 +1,150 @@
+package container
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+type (
+	// BtsBufWriter allows to split provided slice of bytes on to chunks
+	BtsBufWriter struct {
+		buf     []byte
+		clsdPos int
+		offs    int
+	}
+
+	// BtsBufIterator can iterate over slice of bytes which contains some
+	// chunks of bytes and return them
+	BtsBufIterator struct {
+		buf  []byte
+		cur  []byte
+		offs int
+		cnt  int
+	}
+)
+
+// check will make a check if the buf is properly organized and iteratable
+func check(buf []byte) (int, error) {
+	cnt := 0
+	offs := 0
+	for offs < len(buf) {
+		if offs > len(buf)-4 {
+			return -1, errors.New("wrong offset")
+		}
+
+		ln := binary.BigEndian.Uint32(buf[offs:])
+		if ln == 0xFFFFFFFF {
+			offs = len(buf)
+			break
+		}
+		cnt++
+		offs += int(ln) + 4
+	}
+
+	if offs == len(buf) {
+		return cnt, nil
+	}
+
+	return -1, errors.New("broken structure")
+}
+
+// Reset initializes BtsBufIterator and checks whether the provided buf
+// is properly organized. Returns an error if the structure is incorrect.
+// If the method returns an error End() will return true, Get() will return nil
+// and the Next() call will not have any effect
+func (bbi *BtsBufIterator) Reset(buf []byte) error {
+	cnt, err := check(buf)
+	if err != nil {
+		return err
+	}
+	bbi.buf = buf
+	bbi.cur = nil
+	bbi.offs = 0
+	bbi.cnt = cnt
+	bbi.fillCur()
+	return nil
+}
+
+func (bbi *BtsBufIterator) fillCur() {
+	if bbi.offs < len(bbi.buf) {
+		ln := binary.BigEndian.Uint32(bbi.buf[bbi.offs:])
+		if ln != 0xFFFFFFFF {
+			offs := bbi.offs + 4
+			bbi.cur = bbi.buf[offs : offs+int(ln)]
+			return
+		}
+	}
+	bbi.cur = nil
+	bbi.offs = len(bbi.buf)
+}
+
+// End returns true if the iterator reaches the end and doesn't have any data,
+// Get will return nil when End() returns true
+func (bbi *BtsBufIterator) End() bool {
+	return bbi.offs >= len(bbi.buf)
+}
+
+// Get returns current element. Get returns first element after initialization
+func (bbi *BtsBufIterator) Get() []byte {
+	return bbi.cur
+}
+
+// Next switches to the next element. Get() allows to access to the current one.
+// Has no effect if the end is reached
+func (bbi *BtsBufIterator) Next() {
+	bbi.offs += 4 + len(bbi.cur)
+	bbi.fillCur()
+}
+
+// Buf returns underlying buffer
+func (bbi *BtsBufIterator) Buf() []byte {
+	return bbi.buf
+}
+
+// Len returns number of records found in the buf
+func (bbi *BtsBufIterator) Len() int {
+	return bbi.cnt
+}
+
+// Reset Initializes the writer by provided slice.
+func (bbw *BtsBufWriter) Reset(buf []byte) {
+	bbw.buf = buf
+	bbw.offs = 0
+	bbw.clsdPos = -1
+}
+
+// Allocate reserves ln bytes for data and writes its size before the chunk.
+// The method returns slice of allocated bytes. In case of the allocation is not
+// possible, it returns error
+func (bbw *BtsBufWriter) Allocate(ln int) ([]byte, error) {
+	rest := len(bbw.buf) - bbw.offs - ln - 4
+	if rest < 4 && rest != 0 {
+		return nil, errors.New("not enough space")
+	}
+	binary.BigEndian.PutUint32(bbw.buf[bbw.offs:], uint32(ln))
+	bbw.offs += ln + 4
+	return bbw.buf[bbw.offs-ln : bbw.offs], nil
+}
+
+// Close puts EOF marker or completes the writing process. Consequentive
+// Allocate() calls will return errors. The Close() method returns slice of
+// bytes with allocated chunks (without the close marker). The method places the
+// close marker into the original slice of bytes for addressing the case
+// when it is not completely used so the original bbw.buf can be used for iteration
+// with no problems
+func (bbw *BtsBufWriter) Close() ([]byte, error) {
+	if bbw.clsdPos >= 0 {
+		return bbw.buf[:bbw.clsdPos], nil
+	}
+	if bbw.offs == len(bbw.buf) {
+		bbw.clsdPos = bbw.offs
+		return bbw.buf[:bbw.clsdPos], nil
+	}
+	if len(bbw.buf)-bbw.offs < 4 {
+		return nil, errors.New("not enough space")
+	}
+	binary.BigEndian.PutUint32(bbw.buf[bbw.offs:], uint32(0xFFFFFFFF))
+	bbw.clsdPos = bbw.offs
+	bbw.offs = len(bbw.buf)
+	return bbw.buf[:bbw.clsdPos], nil
+}
---
+++ bytes_buf_test.go
@@ -0,0 +1,247 @@
+package container
+
+import (
+	"encoding/binary"
+	"math/rand"
+	"reflect"
+	"testing"
+)
+
+func TestBtsBufWriterEmpty(t *testing.T) {
+	var bbw BtsBufWriter
+	var buf [100]byte
+	bbw.Reset(buf[:])
+	bf, err := bbw.Close()
+	if err != nil || len(bf) != 0 {
+		t.Fatal("should be empty")
+	}
+
+	bf, err = bbw.Close()
+	if err != nil || len(bf) != 0 {
+		t.Fatal("should be empty from second attempt")
+	}
+}
+
+func TestBtsBufWriterAllocate(t *testing.T) {
+	var bbw BtsBufWriter
+	var buf [100]byte
+	bbw.Reset(buf[:])
+	bf, err := bbw.Allocate(20)
+	if bf == nil || len(bf) != 20 || err != nil {
+		t.Fatal("Should be able to allocate 20 bytes err=", err)
+	}
+
+	bf, err = bbw.Allocate(20)
+	if bf == nil || len(bf) != 20 || err != nil {
+		t.Fatal("Should be able to allocate 20 bytes again err=", err)
+	}
+
+	bf, err = bbw.Allocate(60)
+	if err == nil {
+		t.Fatal("Should not be able to allocate 60 bytes err=", err)
+	}
+
+	if bbw.offs != 48 {
+		t.Fatal("expecting offs=48, but it is ", bbw.offs)
+	}
+
+	bf, err = bbw.Close()
+	if err != nil || len(bf) != 48 {
+		t.Fatal("should be 48 bytes lenght")
+	}
+
+	v := binary.BigEndian.Uint32(buf[48:])
+	if v != 0xFFFFFFFF {
+		t.Fatal("No marker")
+	}
+}
+
+func TestInsufficientAllocate(t *testing.T) {
+	var bbw BtsBufWriter
+	var buf [12]byte
+	bbw.Reset(buf[:])
+	bf, err := bbw.Allocate(4)
+	if bf == nil || len(bf) != 4 || err != nil {
+		t.Fatal("Should be able to allocate 4 bytes err=", err)
+	}
+
+	bbw.Reset(buf[:])
+	bf, err = bbw.Allocate(2)
+	if bf == nil || len(bf) != 2 || err != nil {
+		t.Fatal("Should be able to allocate 2 bytes err=", err)
+	}
+
+	bbw.Reset(buf[:])
+	bf, err = bbw.Allocate(8)
+	if bf == nil || len(bf) != 8 || err != nil {
+		t.Fatal("Should be able to allocate 8 bytes err=", err)
+	}
+
+	bbw.Reset(buf[:])
+	bf, err = bbw.Allocate(5)
+	if bf != nil || err == nil {
+		t.Fatal("Should not be able to allocate 5 bytes err=", err)
+	}
+	bbw.Reset(buf[:])
+	bf, err = bbw.Allocate(25)
+	if bf != nil || err == nil {
+		t.Fatal("Should not be able to allocate 25 bytes err=", err)
+	}
+}
+
+func TestEmptyBufIterator(t *testing.T) {
+	var bbi BtsBufIterator
+	if !bbi.End() || bbi.Get() != nil {
+		t.Fatal("Empty iterator check fail")
+	}
+	bbi.Next()
+	if !bbi.End() || bbi.Get() != nil {
+		t.Fatal("Empty iterator check fail (2)")
+	}
+
+	var buf []byte
+	err := bbi.Reset(buf)
+	if err != nil {
+		t.Fatal("should not be problem with empty reset")
+	}
+
+	bbi.Next()
+	if !bbi.End() || bbi.Get() != nil {
+		t.Fatal("Empty iterator check fail (3)")
+	}
+}
+
+func TestResetBtsBufIterator(t *testing.T) {
+	var bbi BtsBufIterator
+	var buf [20]byte
+	err := bbi.Reset(buf[:4])
+	if err != nil {
+		t.Fatal("should not be problem with empty reset, err=", err)
+	}
+	err = bbi.Reset(buf[:])
+	if err != nil {
+		t.Fatal("should not be problem with empty reset(2), err=", err)
+	}
+
+	err = bbi.Reset(buf[:8])
+	if err != nil {
+		t.Fatal("should not be problem with empty reset(3), err=", err)
+	}
+
+	err = bbi.Reset(buf[:7])
+	if err == nil {
+		t.Fatal("should be problem with empty reset, but it is not")
+	}
+	err = bbi.Reset(buf[:2])
+	if err == nil {
+		t.Fatal("should be problem with empty reset, but it is not")
+	}
+
+	binary.BigEndian.PutUint32(buf[:], 3)
+	binary.BigEndian.PutUint32(buf[7:], 0xFFFFFFFF)
+	err = bbi.Reset(buf[:])
+	if err != nil {
+		t.Fatal("should not be problem with empty reset(4), err=", err)
+	}
+
+	binary.BigEndian.PutUint32(buf[7:], 0x12FFFFFF)
+	err = bbi.Reset(buf[:])
+	if err == nil {
+		t.Fatal("should be problem with empty reset, but it is not (2)")
+	}
+}
+
+func TestBtsBufIterator(t *testing.T) {
+	var bbw BtsBufWriter
+	var dst [200]byte
+	var src [92]byte
+
+	rand.Read(src[:])
+	bbw.Reset(dst[:])
+	var sz int
+	for offs := 0; offs < len(src); offs += sz {
+		sz = 20 + offs/10
+		bw, err := bbw.Allocate(sz)
+		if len(bw) != sz || err != nil {
+			t.Fatal("Something goes wrong with allocation err=", err)
+		}
+		copy(bw, src[offs:])
+	}
+
+	bw, err := bbw.Close()
+	if len(bw) != 108 || err != nil {
+		t.Fatal("Something goes wrong with writing")
+	}
+
+	var bbi BtsBufIterator
+	err = bbi.Reset(dst[:])
+	if err != nil {
+		t.Fatal("Expecting no problems with the dst buf, but err=", err)
+	}
+
+	if bbi.Len() != 4 {
+		t.Fatal("Expected len is 4, but got ", bbi.Len())
+	}
+
+	offs := 0
+	for !bbi.End() {
+		bw = bbi.Get()
+		sz = 20 + offs/10
+		if len(bw) != sz {
+			t.Fatal("Wrong buf size ", len(bw), ", but expected ", sz)
+		}
+		if !reflect.DeepEqual(bw, src[offs:offs+sz]) {
+			t.Fatal("wrong data read. Expected size sz=", sz, ", offs=", offs)
+		}
+		offs += sz
+		bbi.Next()
+	}
+}
+
+func TestBtsBufIteratorEven(t *testing.T) {
+	var bbw BtsBufWriter
+	var dst [16]byte
+	var src [8]byte
+
+	rand.Read(src[:])
+	bbw.Reset(dst[:])
+	for offs := 0; offs < len(src); offs += 4 {
+		bw, err := bbw.Allocate(4)
+		if len(bw) != 4 || err != nil {
+			t.Fatal("Something goes wrong with allocation err=", err)
+		}
+		copy(bw, src[offs:])
+	}
+
+	bw, err := bbw.Close()
+	if len(bw) != 16 || err != nil {
+		t.Fatal("Something goes wrong with writing")
+	}
+
+	var bbi BtsBufIterator
+	err = bbi.Reset(dst[:])
+	if err != nil {
+		t.Fatal("Expecting no problems with the dst buf, but err=", err)
+	}
+	if bbi.Len() != 2 {
+		t.Fatal("Expected len is 2, but got ", bbi.Len())
+	}
+
+	offs := 0
+	its := 0
+	for !bbi.End() {
+		its++
+		bw = bbi.Get()
+		if len(bw) != 4 {
+			t.Fatal("Wrong buf size ", len(bw), ", but expected 4")
+		}
+		if !reflect.DeepEqual(bw, src[offs:offs+4]) {
+			t.Fatal("wrong data read. Expected size 4, offs=", offs)
+		}
+		offs += 4
+		bbi.Next()
+	}
+	if its != 2 {
+		t.Fatal("should be iterate over 2 elems")
+	}
+}
